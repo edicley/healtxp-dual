41378224e22a30aeeec85a16a317ab2a5209c196
Edicley Bezerra <edyhenrique@gmail.com>
2023-07-23 16:56:19 -0300

    Criando command popUpHave

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -28,14 +28,20 @@
 Cypress.Commands.add('doLogin', (user) => {
     cy.visit('http://localhost:3000')
 
-    if(user.email) {
+    if (user.email) {
         cy.get('input[name=email]').type(user.email)
     }
 
-    if(user.password) {
+    if (user.password) {
         cy.get('input[name=password]').type(user.password)
-    }   
+    }
 
     cy.contains('button', 'Entrar')
         .click()
+})
+
+Cypress.Commands.add('popUpHave', (text) => {
+    cy.get('#swal2-content')
+        .should('be.visible')
+        .should('have.text', text)
 })
