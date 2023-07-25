dba80c4d11cdbc1b5b2675e0943ffd045f3ec654
Edicley Bezerra <edyhenrique@gmail.com>
2023-07-25 20:47:16 -0300

    Removendo implementação do fakerJS

--- web/cypress/e2e/students.cy.js
+++ web/cypress/e2e/students.cy.js
@@ -2,22 +2,12 @@ import students from '../fixtures/students.json'
 
 import dash from '../support/pages/DashPage'
 
-import { faker } from '@faker-js/faker'
-
 describe('students', () => {
 
     it('deve poder cadastrar um novo aluno', () => {
 
-        //const student = students.create
-        const student = {
-            name: 'Jeremy Adams',
-            email: faker.internet.email(),
-            age: '28',
-            weight: '72',
-            feet_tall: '1.80'
-        }
-
-
+        const student = students.create
+        
         cy.adminLogin()
 
         cy.contains('a', 'Cadastrar').click()
