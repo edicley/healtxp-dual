cae981aa6cbf0b15e6e16145c2a81bf982eca186
Edicley Bezerra <edyhenrique@gmail.com>
2023-07-25 07:57:33 -0300

    Criando classe DashPage

---
+++ web/cypress/support/pages/DashPage.js
@@ -0,0 +1,11 @@
+
+
+class DashPage {
+
+    userLoggedIn(name) {
+        cy.contains('aside .logged-user', 'Olá, ' + name)
+            .should('be.visible')
+    }
+}
+
+export default new DashPage()
